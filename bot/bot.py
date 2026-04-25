8ac7b09daf544724a631801fd43a036d7ef8a1e4
Сергей Аренков <sergejarenkov@gmail.com>
2026-04-25 10:35:37 +0300

    Добавлен TTS

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import db
 import func
 import re
+from silero import silero_tts
+from playsound3 import playsound
 
 if __name__ == "__main__":
     db.init_db()
@@ -11,6 +13,13 @@ if __name__ == "__main__":
 
         response = func.process_message(user_input)
 
+        model, text = silero_tts(language='ru',
+                                 speaker='v5_ru')
+        audio = model.save_wav(text=response,
+                             speaker="eugene",
+                             sample_rate=48000)
+
+        playsound("test.wav")
         db.log_message(now, user_input, response)
         print(f"{now} Бот: {response}")
 
